60bbd13554e87010271d488cada21a3bd86620e6
Gaetanerk <gaetan.turben@outlook.com>
2022-04-26 13:44:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let tableau = [5, 15, 26, 12, 78, 56, 45, 78, 42];
+// 1
+console.log(tableau);
+// document.querySelector("#tableau1").innerHTML = tableau;
+
+// 2
+// let user = prompt("Entrez une valeur");
+// tableau.push(user);
+// document.querySelector("#tableau2").innerHTML = tableau;
+
+// 3
+// document.querySelector("#tableau3").innerHTML = tableau;
+// tableau[2] = 42;
+// tableau[8] = 26;
+// document.querySelector("#tableau4").innerHTML = tableau;
+
+// 4
+// document.querySelector("#tableau5").innerHTML = tableau[2];
+// document.querySelector("#tableau6").innerHTML = tableau[6];
+
+// 5
+// document.querySelector("#tableau7").innerHTML = tableau;
+// tableau.splice(4, 1);
+// document.querySelector("#tableau8").innerHTML = tableau;
+
+// 6
+let copieTableau = tableau;
+document.querySelector("#tableau9").innerHTML = tableau;
+document.querySelector("#tableau10").innerHTML = copieTableau;
